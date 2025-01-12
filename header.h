43c1673d2fa53916bab3658dcc45dc2d45d97b00
ravensramen <153202736+ravensramen@users.noreply.github.com>
2025-01-12 08:04:31 -0800

    header.h

---
+++ header.h
@@ -0,0 +1,17 @@
+//Name:
+//Course:
+//Professor + TA:
+//Assignment:
+//Date:
+
+//preprocessor directives
+#define _CRT_SECURE_NO_WARNINGS
+
+//import libraries for functions
+#include <stdio.h> //printf, scanf, file functions...
+#include <string.h> //string mod functions, strtok, strlen...
+#include <stdbool.h> //for boolean functions
+
+//function declarations, descriptions in functions.c
+bool checkSuccess(FILE* input);
+void welcomeStatement(void);
